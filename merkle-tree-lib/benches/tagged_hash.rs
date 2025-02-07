f17ab7d938b35b6ef2e2712a0e13ee3b2407b557
msakuta <masahiro.sakuta@gmail.com>
2025-02-07 22:42:54 +0900

    Use flat vec as memory arena and fixed size array for hash

--- merkle-tree-lib/benches/tagged_hash.rs
+++ merkle-tree-lib/benches/tagged_hash.rs
@@ -8,15 +8,19 @@ fn bench_tagged_hash(c: &mut Criterion) {
     let mut group = c.benchmark_group("merkle_tree_lib::tagged_hash");
 
     for max_range in [1, 10, 100, 1_000, 10_000, 100_000, 1_000_000].iter() {
-        group.bench_with_input(BenchmarkId::from_parameter(max_range), max_range, |b, &max_range| {
-            b.iter(|| {
-                std::hint::black_box({
-                    for _ in 0..max_range {
-                        merkle_tree_lib::tagged_hash(&tag_leaf, "aaa".as_bytes());
-                    }
+        group.bench_with_input(
+            BenchmarkId::from_parameter(max_range),
+            max_range,
+            |b, &max_range| {
+                b.iter(|| {
+                    std::hint::black_box({
+                        for _ in 0..max_range {
+                            merkle_tree_lib::tagged_hash(&tag_leaf, "aaa".as_bytes());
+                        }
+                    });
                 });
-            });
-        });
+            },
+        );
     }
 
     group.finish();
